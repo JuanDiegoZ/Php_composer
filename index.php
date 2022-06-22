671bd7d55df0cf30f33790a27f7c2fb05ab82178
Juan DIego Zipaquira A <juan444j@gmail.com>
2022-06-22 15:19:57 -0500

    sintaxis basica en Php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+class Answer
+{
+    protected $clients = []; //My note
+    protected $insurers = []; // My note
+}
+
+?>
+
+// Sintaxis basica php.
+
+// Asignacion
+
+$num = 9;
+$lang =  [
+    'es'  = 'español',
+    'en'  = 'english'
+    ];
+
+//aritmetica
+
+echo "Suma 2 + 2 ".((int) 2 + (int) 2);
+echo "Suma 2 - 2 ".((int) 2 - (int) 2);
+echo "Suma 2 * 2 ". 2 * 2;
+echo "Suma 2 + 2 ". 2 / 2;
+echo "Modulo 2 % 2". 2 % 2;
